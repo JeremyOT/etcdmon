ebebd992f2f8e39628cdecce7fd6811654d04552
Jeremy Olmsted-Thompson <jeremyot@gmail.com>
2014-08-03 18:52:31 -0700

    gofmt

--- etcdmon.go
+++ etcdmon.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-  "os"
-  "flag"
-  "fmt"
-  "strings"
-  "log"
-  "time"
-  "path"
-  "strconv"
-  "github.com/JeremyOT/etcdmon/cmd"
-  "github.com/JeremyOT/etcdmon/etcd"
-  )
+	"flag"
+	"fmt"
+	"github.com/JeremyOT/etcdmon/cmd"
+	"github.com/JeremyOT/etcdmon/etcd"
+	"log"
+	"os"
+	"path"
+	"strconv"
+	"strings"
+	"time"
+)
 
 var etcAddress = flag.String("etcd", "http://127.0.0.1:4001", "The url of the etcd instance to connect to.")
 var etcKey = flag.String("key", "", "The key path to post to. %H will be replaced with the current host and %P with any configured port. e.g. process/%H:%P")
@@ -24,61 +24,61 @@ var apiRoot = flag.String("api", "/v2/keys", "The root value for any key path.")
 var getHost = flag.Bool("gethost", false, "Print the current hostname (using -etcd for lookup) and exit.")
 
 func formatValue(value, host string, port int) string {
-  if value == "" {
-    if port > 0 {
-      return fmt.Sprintf("%s:%d", host, port)
-    } else {
-      return host
-    }
-  }
-  return strings.Replace(strings.Replace(value, "%H", host, -1), "%P", strconv.Itoa(port), -1)
+	if value == "" {
+		if port > 0 {
+			return fmt.Sprintf("%s:%d", host, port)
+		} else {
+			return host
+		}
+	}
+	return strings.Replace(strings.Replace(value, "%H", host, -1), "%P", strconv.Itoa(port), -1)
 }
 
 func formatKey(key, host string, port int) string {
-  return strings.Replace(strings.Replace(key, "%H", host, -1), "%P", strconv.Itoa(port), -1)
+	return strings.Replace(strings.Replace(key, "%H", host, -1), "%P", strconv.Itoa(port), -1)
 }
 
 func main() {
-  flag.Parse()
-  if *getHost {
-    if serviceHost, err := etcd.LocalHost(*etcAddress); err != nil {
-      log.Println("Error retrieving host:", err)
-      os.Exit(1)
-    } else {
-      fmt.Print(serviceHost);
-      os.Exit(0)
-    }
-  }
-  args := flag.Args()
-  if *etcAddress == "" || *etcKey == "" {
-    fmt.Println("Usage:")
-    fmt.Println("  Updates -key in etcd periodically until either exited or a monitored process exits.")
-    fmt.Println("  To monitor an external command add the command argurments after all etcdmon options.")
-    fmt.Println("  E.g. etcdmon -key process -- my_script.sh arg1 arg2")
-    fmt.Println("  stdin, stdout and stderr will be piped through from the monitored command.")
-    fmt.Println()
-    fmt.Println("Options:")
-    flag.PrintDefaults()
-    os.Exit(1)
-  }
-  serviceHost := *host
-  if serviceHost == "" {
-    var err error
-    serviceHost, err = etcd.LocalHost(*etcAddress)
-    if err != nil {
-      log.Println("Error retrieving host. Try setting it manually with -host=''.", err)
-      os.Exit(1)
-    }
-  }
-  keyPath := formatKey(path.Join(*apiRoot, *etcKey), serviceHost, *port)
-  formattedValue := formatValue(*value, serviceHost, *port)
-  fmt.Println("Etcd:", *etcAddress, "Key:", keyPath, "Value:", formattedValue, "TTL:", *ttl, "UpdateInterval:", *updateInterval)
-  quit := make(chan int)
-  if len(args) == 0 {
-    fmt.Println("No command specified. Running indefinitely.")
-  } else {
-    fmt.Println("Monitoring command:", strings.Join(args, " "))
-    go cmd.RunCommand(args, quit)
-  }
-  etcd.RegisterService(*etcAddress, keyPath, formattedValue, time.Duration(*ttl) * time.Second, time.Duration(*updateInterval) * time.Second, quit)
+	flag.Parse()
+	if *getHost {
+		if serviceHost, err := etcd.LocalHost(*etcAddress); err != nil {
+			log.Println("Error retrieving host:", err)
+			os.Exit(1)
+		} else {
+			fmt.Print(serviceHost)
+			os.Exit(0)
+		}
+	}
+	args := flag.Args()
+	if *etcAddress == "" || *etcKey == "" {
+		fmt.Println("Usage:")
+		fmt.Println("  Updates -key in etcd periodically until either exited or a monitored process exits.")
+		fmt.Println("  To monitor an external command add the command argurments after all etcdmon options.")
+		fmt.Println("  E.g. etcdmon -key process -- my_script.sh arg1 arg2")
+		fmt.Println("  stdin, stdout and stderr will be piped through from the monitored command.")
+		fmt.Println()
+		fmt.Println("Options:")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	serviceHost := *host
+	if serviceHost == "" {
+		var err error
+		serviceHost, err = etcd.LocalHost(*etcAddress)
+		if err != nil {
+			log.Println("Error retrieving host. Try setting it manually with -host=''.", err)
+			os.Exit(1)
+		}
+	}
+	keyPath := formatKey(path.Join(*apiRoot, *etcKey), serviceHost, *port)
+	formattedValue := formatValue(*value, serviceHost, *port)
+	fmt.Println("Etcd:", *etcAddress, "Key:", keyPath, "Value:", formattedValue, "TTL:", *ttl, "UpdateInterval:", *updateInterval)
+	quit := make(chan int)
+	if len(args) == 0 {
+		fmt.Println("No command specified. Running indefinitely.")
+	} else {
+		fmt.Println("Monitoring command:", strings.Join(args, " "))
+		go cmd.RunCommand(args, quit)
+	}
+	etcd.RegisterService(*etcAddress, keyPath, formattedValue, time.Duration(*ttl)*time.Second, time.Duration(*updateInterval)*time.Second, quit)
 }
