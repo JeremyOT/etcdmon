b58e22d329a1ed6a651e97a462dba3f8a2d3f2b4
Jeremy Olmsted-Thompson <jeremyot@gmail.com>
2014-08-05 21:34:49 -0700

    add -remote option

--- etcdmon.go
+++ etcdmon.go
@@ -13,15 +13,16 @@ import (
 	"time"
 )
 
-var etcAddress = flag.String("etcd", "http://127.0.0.1:4001", "The url of the etcd instance to connect to.")
-var etcKey = flag.String("key", "", "The key path to post to. %H will be replaced with the current host and %P with any configured port. e.g. process/%H:%P")
+var etcdAddress = flag.String("etcd", "http://127.0.0.1:4001", "The url of the etcd instance to connect to.")
+var etcdKey = flag.String("key", "", "The key path to post to. %H will be replaced with the current host and %P with any configured port. e.g. process/%H:%P")
 var updateInterval = flag.Int("interval", 10, "The number of seconds between each poll to etcd.")
 var ttl = flag.Int("ttl", 30, "The number of seconds that the key should stay alive after no polls are received.")
 var host = flag.String("host", "", "The that should be used to identify this daemon. If not set, etcdmon will attempt to determine it automatically.")
 var port = flag.Int("port", 0, "A port, if any that will be set along with the host name.")
 var value = flag.String("value", "", "The the value to sent to etcd. Like path, %H and %P can be used for automatic replacement. If not set, the host (and port if set) will be used as the value.")
 var apiRoot = flag.String("api", "/v2/keys", "The root value for any key path.")
-var getHost = flag.Bool("gethost", false, "Print the current hostname (using -etcd for lookup) and exit.")
+var getHost = flag.Bool("gethost", false, "Print the current hostname (using -remote for lookup) and exit.")
+var remoteTestAddress = flag.String("remote", "", "The address to use to infer the address of the local host.")
 
 func formatValue(value, host string, port int) string {
 	if value == "" {
@@ -40,8 +41,12 @@ func formatKey(key, host string, port int) string {
 
 func main() {
 	flag.Parse()
+	remote := *remoteTestAddress
+	if remote == "" {
+		remote = *etcdAddress
+	}
 	if *getHost {
-		if serviceHost, err := etcd.LocalHost(*etcAddress); err != nil {
+		if serviceHost, err := etcd.LocalHost(remote); err != nil {
 			log.Println("Error retrieving host:", err)
 			os.Exit(1)
 		} else {
@@ -50,7 +55,7 @@ func main() {
 		}
 	}
 	args := flag.Args()
-	if *etcAddress == "" || *etcKey == "" {
+	if *etcdAddress == "" || *etcdKey == "" {
 		fmt.Println("Usage:")
 		fmt.Println("  Updates -key in etcd periodically until either exited or a monitored process exits.")
 		fmt.Println("  To monitor an external command add the command argurments after all etcdmon options.")
@@ -64,15 +69,15 @@ func main() {
 	serviceHost := *host
 	if serviceHost == "" {
 		var err error
-		serviceHost, err = etcd.LocalHost(*etcAddress)
+		serviceHost, err = etcd.LocalHost(remote)
 		if err != nil {
-			log.Println("Error retrieving host. Try setting it manually with -host=''.", err)
+			log.Println("Error retrieving host. Try using a different -remote='' or setting it manually with -host=''.", err)
 			os.Exit(1)
 		}
 	}
-	keyPath := formatKey(path.Join(*apiRoot, *etcKey), serviceHost, *port)
+	keyPath := formatKey(path.Join(*apiRoot, *etcdKey), serviceHost, *port)
 	formattedValue := formatValue(*value, serviceHost, *port)
-	fmt.Println("Etcd:", *etcAddress, "Key:", keyPath, "Value:", formattedValue, "TTL:", *ttl, "UpdateInterval:", *updateInterval)
+	fmt.Println("Etcd:", *etcdAddress, "Key:", keyPath, "Value:", formattedValue, "TTL:", *ttl, "UpdateInterval:", *updateInterval)
 	quit := make(chan int)
 	if len(args) == 0 {
 		fmt.Println("No command specified. Running indefinitely.")
@@ -80,5 +85,5 @@ func main() {
 		fmt.Println("Monitoring command:", strings.Join(args, " "))
 		go cmd.RunCommand(args, quit)
 	}
-	etcd.RegisterService(*etcAddress, keyPath, formattedValue, time.Duration(*ttl)*time.Second, time.Duration(*updateInterval)*time.Second, quit)
+	etcd.RegisterService(*etcdAddress, keyPath, formattedValue, time.Duration(*ttl)*time.Second, time.Duration(*updateInterval)*time.Second, quit)
 }
